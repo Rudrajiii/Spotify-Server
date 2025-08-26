74bb5e75078f8f14f1859f54e9ebe8b8e303f383
rudrajiii <rudrasaha305@gmail.com>
2025-08-26 18:17:50 +0530

    cors added for tech domain

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ const app = express();
  * @cors setup
  */
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://rudyy.vercel.app'],
+  origin: [
+          'http://localhost:5173',
+          'https://rudyy.vercel.app',
+          'https://rudyy.tech',    
+          'https://www.rudyy.tech' 
+        ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'If-None-Match'],
   exposedHeaders: ['ETag', 'Cache-Control'],
